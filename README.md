57c770f59b685705b39b3ea862a826807fbd9bbb
FlacoJones <andrew@openq.dev>
2021-11-19 13:59:46 -0600

    update Issue entity

---
+++ README.md
@@ -0,0 +1,8 @@
+# OpenQ-Graph
+
+`yarn codegen`: Creates a `generated` directory with TypeScript bindings for your contract
+
+`yarn deploy`: Create a `build` directory with your WASM compiled Graph node indexer and GraphQL API
+
+Queries (HTTP):     https://api.thegraph.com/subgraphs/name/openqdev/openq
+Subscriptions (WS): wss://api.thegraph.com/subgraphs/name/openqdev/openq
---
+++ src/mapping.ts
@@ -0,0 +1,54 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { OpenQ, IssueClosed, IssueCreated } from "../generated/OpenQ/OpenQ"
+import { Issue } from "../generated/schema"
+
+export function handleIssueCreated(event: IssueCreated): void {
+	// Entities can be loaded from the store using a string ID; this ID
+	// needs to be unique across all entities of the same type
+	let entity = Issue.load(event.transaction.from.toHex())
+
+	// Entities only exist after they have been saved to the store;
+	// `null` checks allow to create entities on demand
+	if (!entity) {
+		entity = new Issue(event.transaction.from.toHex())
+
+		// Entity fields can be set using simple assignments
+		entity.count = BigInt.fromI32(0)
+	}
+
+	// BigInt and BigDecimal math are supported
+	entity.count = entity.count + BigInt.fromI32(1)
+
+	// Entity fields can be set based on event parameters
+	entity.id = event.params.issueId
+	entity.issueAddress = event.params.issueAddress
+
+	// Entities can be written to the store with `.save()`
+	entity.save()
+
+	// Note: If a handler doesn't require existing field values, it is faster
+	// _not_ to load the entity from the store. Instead, create it fresh with
+	// `new Entity(...)`, set the fields that should be updated and save the
+	// entity back to the store. Fields that were not set or unset remain
+	// unchanged, allowing for partial updates to be applied.
+
+	// It is also possible to access smart contracts from mappings. For
+	// example, the contract that has emitted the event can be connected to
+	// with:
+	//
+	// let contract = Contract.bind(event.address)
+	//
+	// The following functions can then be called on this contract to access
+	// state variables and other data:
+	//
+	// - contract.addressToIssue(...)
+	// - contract.getBountyAddress(...)
+	// - contract.getIssueIds(...)
+	// - contract.issueIds(...)
+	// - contract.issueIsOpen(...)
+	// - contract.issueToAddress(...)
+	// - contract.mintBounty(...)
+	// - contract.tokenAddresses(...)
+}
+
+export function handleIssueClosed(event: IssueClosed): void { }
