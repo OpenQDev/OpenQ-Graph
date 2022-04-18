29e72785091fa2d3cb0b7d7af780844d7db401b7
FlacoJones <andrew@openq.dev>
2022-04-17 20:00:45 -0500

    removes .claimed from Deposit

--- src/mappings/handleTokenBalanceClaimed.ts
+++ src/mappings/handleTokenBalanceClaimed.ts
@@ -1,5 +1,5 @@
 import { BigInt, store } from "@graphprotocol/graph-ts"
-import { DepositClaimed } from "../../generated/OpenQ/OpenQ"
+import { TokenBalanceClaimed } from "../../generated/OpenQ/OpenQ"
 import {
 	TokenEvents,
 	Payout,
@@ -7,26 +7,16 @@ import {
 	UserPayoutTokenBalance,
 	OrganizationPayoutTokenBalance,
 	OrganizationFundedTokenBalance,
-	PayoutTokenBalance,
-	Deposit
+	PayoutTokenBalance
 } from "../../generated/schema"
 
-export default function handleDepositClaimed(event: DepositClaimed): void {
-	// UPDATE DEPOSIT CLAIMED
-	let deposit = Deposit.load(event.params.depositId.toHexString())
-
-	if (!deposit) {
-		throw "Error"
-	}
-
-	deposit.claimed = true;
-
-	let bountyPayoutId = `${event.params.closer.toHexString()}-${event.params.bountyAddress.toHexString()}-${deposit.tokenAddress.toHexString()}-${event.params.payoutTime}`
+export default function handleTokenBalanceClaimed(event: TokenBalanceClaimed): void {
+	let bountyPayoutId = `${event.params.closer.toHexString()}-${event.params.bountyAddress.toHexString()}-${event.params.tokenAddress.toHexString()}-${event.params.payoutTime}`
 	let payout = new Payout(bountyPayoutId)
 
-	payout.tokenAddress = deposit.tokenAddress
+	payout.tokenAddress = event.params.tokenAddress
 	payout.bounty = event.params.bountyAddress.toHexString()
-	payout.volume = deposit.volume
+	payout.volume = event.params.volume
 	payout.payoutTime = event.params.payoutTime
 	payout.organization = event.params.organization
 	payout.transactionHash = event.transaction.hash;
@@ -42,62 +32,61 @@ export default function handleDepositClaimed(event: DepositClaimed): void {
 	payout.closer = user.id
 
 	// UPSERT TOKEN EVENTS
-	let tokenEvents = TokenEvents.load(deposit.tokenAddress.toHexString())
+	let tokenEvents = TokenEvents.load(event.params.tokenAddress.toHexString())
 
 	if (!tokenEvents) {
-		tokenEvents = new TokenEvents(deposit.tokenAddress.toHexString())
+		tokenEvents = new TokenEvents(event.params.tokenAddress.toHexString())
 		payout.tokenEvents = tokenEvents.id
 	}
 
 	// UPSERT USER PAYOUT TOKEN BALANCE
-	const userPayoutTokenBalanceId = `${event.params.closer.toHexString()}-${deposit.tokenAddress.toHexString()}`
+	const userPayoutTokenBalanceId = `${event.params.closer.toHexString()}-${event.params.tokenAddress.toHexString()}`
 	let userPayoutTokenBalance = UserPayoutTokenBalance.load(userPayoutTokenBalanceId)
 
 	if (!userPayoutTokenBalance) {
 		userPayoutTokenBalance = new UserPayoutTokenBalance(userPayoutTokenBalanceId)
 		userPayoutTokenBalance.user = event.params.closer.toHexString()
-		userPayoutTokenBalance.tokenAddress = deposit.tokenAddress
+		userPayoutTokenBalance.tokenAddress = event.params.tokenAddress
 	}
 
-	userPayoutTokenBalance.volume = userPayoutTokenBalance.volume.plus(deposit.volume)
+	userPayoutTokenBalance.volume = userPayoutTokenBalance.volume.plus(event.params.volume)
 
 	// UPSERT ORGANIZATION PAYOUT TOKEN BALANCE
-	const organizationPayoutTokenBalanceId = `${event.params.organization}-${deposit.tokenAddress.toHexString()}`
+	const organizationPayoutTokenBalanceId = `${event.params.organization}-${event.params.tokenAddress.toHexString()}`
 	let organizationPayoutTokenBalance = OrganizationPayoutTokenBalance.load(organizationPayoutTokenBalanceId)
 
 	if (!organizationPayoutTokenBalance) {
 		organizationPayoutTokenBalance = new OrganizationPayoutTokenBalance(organizationPayoutTokenBalanceId)
 		organizationPayoutTokenBalance.organization = event.params.organization
-		organizationPayoutTokenBalance.tokenAddress = deposit.tokenAddress
+		organizationPayoutTokenBalance.tokenAddress = event.params.tokenAddress
 	}
 
-	organizationPayoutTokenBalance.volume = organizationPayoutTokenBalance.volume.plus(deposit.volume)
+	organizationPayoutTokenBalance.volume = organizationPayoutTokenBalance.volume.plus(event.params.volume)
 
 	// UPDATE ORGANIZATION FUNDED TOKEN BALANCE
-	const organizationFundedTokenBalanceId = `${event.params.organization}-${deposit.tokenAddress.toHexString()}`
+	const organizationFundedTokenBalanceId = `${event.params.organization}-${event.params.tokenAddress.toHexString()}`
 	let organizationFundedTokenBalance = OrganizationFundedTokenBalance.load(organizationFundedTokenBalanceId)
 
 	if (!organizationFundedTokenBalance) {
 		throw "Error"
 	}
 
-	organizationFundedTokenBalance.volume = organizationFundedTokenBalance.volume.minus(deposit.volume)
+	organizationFundedTokenBalance.volume = organizationFundedTokenBalance.volume.minus(event.params.volume)
 
 	// UPSERT TOTAL FUNDED TOKEN BALANCE
-	let payoutTokenBalance = PayoutTokenBalance.load(deposit.tokenAddress.toHexString())
+	let payoutTokenBalance = PayoutTokenBalance.load(event.params.tokenAddress.toHexString())
 
 	if (!payoutTokenBalance) {
-		payoutTokenBalance = new PayoutTokenBalance(deposit.tokenAddress.toHexString())
+		payoutTokenBalance = new PayoutTokenBalance(event.params.tokenAddress.toHexString())
 	}
 
-	payoutTokenBalance.volume = payoutTokenBalance.volume.plus(deposit.volume)
+	payoutTokenBalance.volume = payoutTokenBalance.volume.plus(event.params.volume)
 
 	// SAVE ALL ENTITIES
 	payout.save()
 	tokenEvents.save()
 	userPayoutTokenBalance.save()
 	payoutTokenBalance.save()
-	deposit.save()
 	organizationFundedTokenBalance.save()
 
 	if (organizationFundedTokenBalance.volume.equals(new BigInt(0))) {
