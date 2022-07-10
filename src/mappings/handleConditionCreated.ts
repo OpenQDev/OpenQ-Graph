46d9629b6d60d9f92ff88af82d51f5a9992f4e66
FlacoJones <andrew@openq.dev>
2022-07-10 14:50:15 +0200

    adds metrics entity

--- src/mappings/handleConditionCreated.ts
+++ src/mappings/handleConditionCreated.ts
@@ -1,14 +1,28 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import { ConditionCreated } from "../../generated/Core/Core"
 import {
-	User,
-	Condition
+	Condition,
+	Metric
 } from "../../generated/schema"
 
 export default function handleConditionCreated(event: ConditionCreated): void {
-	let user = User.load(event.transaction.from.toHexString())
+	let condition = Condition.load(event.params.conditionId.toString())
 
-	if (!user) {
-		user = new User(event.transaction.from.toHexString())
-		user.save()
+	if (!condition) {
+		condition = new Condition(event.params.conditionId.toString())
+		condition.save()
 	}
+
+	condition.timestamp = event.params.timestamp
+	condition.save()
+
+	let metrics = Metric.load('metrics')
+
+	if (!metrics) {
+		metrics = new Metric('metrics')
+		metrics.save()
+	}
+
+	metrics.count = metrics.count.plus(BigInt.fromString('1'))
+	metrics.save()
 }
