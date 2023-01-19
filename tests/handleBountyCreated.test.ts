6b43acc3f5f81b7a3a95cba0164455dfea9433b7
FlacoJones <andrew@openq.dev>
2023-01-18 19:36:31 -0600

    completes version 4 handle bounty created tests

--- tests/handleBountyCreated.test.ts
+++ tests/handleBountyCreated.test.ts
@@ -226,6 +226,52 @@ describe('handleBountyCreated', () => {
 		assert.fieldEquals('BountiesCounter', Constants.bountiesCounterId, 'count', '1')
 	})
 
+	test('can handle new BountyCreated - ONGOING - VERSION 4', () => {
+		// ARRANGE
+		let newBountyCreatedEvent = createNewBountyCreatedEvent(
+			Constants.bountyId,
+			Constants.organization,
+			Constants.userId,
+			Constants.bountyAddress,
+			Constants.bountyMintTime,
+			Constants.bountyType_ONGOING,
+			Constants.initData_ONGOING_VERSION_4,
+			Constants.VERSION_4
+		)
+
+		newBountyCreatedEvent.transaction.hash = Bytes.fromHexString(Constants.transactionHash)
+		newBountyCreatedEvent.transaction.from = Address.fromString(Constants.userId)
+
+		// ACT
+		handleBountyCreated(newBountyCreatedEvent)
+
+		// ASSERT
+		assert.fieldEquals('Bounty', Constants.id, 'bountyId', Constants.bountyId)
+		assert.fieldEquals('Bounty', Constants.id, 'organization', Constants.organization)
+		assert.fieldEquals('Bounty', Constants.id, 'issuer', Constants.userId)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyAddress', Constants.id)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyMintTime', Constants.bountyMintTime)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyType', Constants.bountyType_ONGOING)
+		assert.fieldEquals('Bounty', Constants.id, 'version', Constants.VERSION_4)
+		assert.fieldEquals('Bounty', Constants.id, 'transactionHash', Constants.transactionHash)
+		assert.fieldEquals('Bounty', Constants.id, 'payoutTokenAddress', Constants.fundingGoalTokenAddress)
+		assert.fieldEquals('Bounty', Constants.id, 'payoutTokenVolume', Constants.fundingGoalVolume)
+		assert.fieldEquals('Bounty', Constants.id, 'hasFundingGoal', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'fundingGoalTokenAddress', Constants.fundingGoalTokenAddress)
+		assert.fieldEquals('Bounty', Constants.id, 'fundingGoalVolume', Constants.fundingGoalVolume)
+		assert.fieldEquals('Bounty', Constants.id, 'invoiceable', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'kycRequired', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'supportingDocuments', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'externalUserId', Constants.externalUserId)
+
+		assert.fieldEquals('User', Constants.userId, 'id', Constants.userId)
+
+		assert.fieldEquals('Organization', Constants.organization, 'id', Constants.organization)
+		assert.fieldEquals('Organization', Constants.organization, 'bountiesCount', '1')
+
+		assert.fieldEquals('BountiesCounter', Constants.bountiesCounterId, 'count', '1')
+	})
+
 	test('can handle new BountyCreated - TIERED', () => {
 		// ARRANGE
 		let newBountyCreatedEvent = createNewBountyCreatedEvent(
@@ -314,6 +360,53 @@ describe('handleBountyCreated', () => {
 		assert.fieldEquals('BountiesCounter', Constants.bountiesCounterId, 'count', '1')
 	})
 
+	test('can handle new BountyCreated - TIERED - VERSION 4', () => {
+		// ARRANGE
+		let newBountyCreatedEvent = createNewBountyCreatedEvent(
+			Constants.bountyId,
+			Constants.organization,
+			Constants.userId,
+			Constants.bountyAddress,
+			Constants.bountyMintTime,
+			Constants.bountyType_TIERED,
+			Constants.initData_TIERED_VERSION_4,
+			Constants.VERSION_4
+		)
+
+		newBountyCreatedEvent.transaction.hash = Bytes.fromHexString(Constants.transactionHash)
+		newBountyCreatedEvent.transaction.from = Address.fromString(Constants.userId)
+
+		// ACT
+		handleBountyCreated(newBountyCreatedEvent)
+
+		// ASSERT
+		assert.fieldEquals('Bounty', Constants.id, 'bountyId', Constants.bountyId)
+		assert.fieldEquals('Bounty', Constants.id, 'organization', Constants.organization)
+		assert.fieldEquals('Bounty', Constants.id, 'issuer', Constants.userId)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyAddress', Constants.id)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyMintTime', Constants.bountyMintTime)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyType', Constants.bountyType_TIERED)
+		assert.fieldEquals('Bounty', Constants.id, 'version', Constants.VERSION_4)
+		assert.fieldEquals('Bounty', Constants.id, 'transactionHash', Constants.transactionHash)
+		assert.fieldEquals('Bounty', Constants.id, 'hasFundingGoal', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'fundingGoalTokenAddress', Constants.fundingGoalTokenAddress)
+		assert.fieldEquals('Bounty', Constants.id, 'fundingGoalVolume', Constants.fundingGoalVolume)
+		assert.fieldEquals('Bounty', Constants.id, 'invoiceable', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'kycRequired', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'supportingDocuments', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'externalUserId', Constants.externalUserId)
+
+		// This is brittle - relies on proper spacing in the stringified array
+		assert.fieldEquals('Bounty', Constants.id, 'payoutSchedule', `[${Constants.payoutSchedule[0]}, ${Constants.payoutSchedule[1]}]`)
+
+		assert.fieldEquals('User', Constants.userId, 'id', Constants.userId)
+
+		assert.fieldEquals('Organization', Constants.organization, 'id', Constants.organization)
+		assert.fieldEquals('Organization', Constants.organization, 'bountiesCount', '1')
+
+		assert.fieldEquals('BountiesCounter', Constants.bountiesCounterId, 'count', '1')
+	})
+
 	test('can handle new BountyCreated - TIERED FIXED', () => {
 		// ARRANGE
 		let newBountyCreatedEvent = createNewBountyCreatedEvent(
@@ -388,6 +481,52 @@ describe('handleBountyCreated', () => {
 		assert.fieldEquals('Bounty', Constants.id, 'payoutTokenAddress', Constants.payoutTokenAddress)
 		assert.fieldEquals('Bounty', Constants.id, 'invoiceable', 'true')
 		assert.fieldEquals('Bounty', Constants.id, 'kycRequired', 'true')
+
+		// This is brittle - relies on proper spacing in the stringified array
+		assert.fieldEquals('Bounty', Constants.id, 'payoutSchedule', `[${Constants.payoutSchedule[0]}, ${Constants.payoutSchedule[1]}]`)
+
+		assert.fieldEquals('User', Constants.userId, 'id', Constants.userId)
+
+		assert.fieldEquals('Organization', Constants.organization, 'id', Constants.organization)
+		assert.fieldEquals('Organization', Constants.organization, 'bountiesCount', '1')
+
+		assert.fieldEquals('BountiesCounter', Constants.bountiesCounterId, 'count', '1')
+	})
+
+	test('can handle new BountyCreated - TIERED FIXED - VERSION 4', () => {
+		// ARRANGE
+		let newBountyCreatedEvent = createNewBountyCreatedEvent(
+			Constants.bountyId,
+			Constants.organization,
+			Constants.userId,
+			Constants.bountyAddress,
+			Constants.bountyMintTime,
+			Constants.bountyType_TIERED_FIXED,
+			Constants.initData_TIERED_FIXED_VERSION_4,
+			Constants.VERSION_4
+		)
+
+		newBountyCreatedEvent.transaction.hash = Bytes.fromHexString(Constants.transactionHash)
+		newBountyCreatedEvent.transaction.from = Address.fromString(Constants.userId)
+
+		// ACT
+		handleBountyCreated(newBountyCreatedEvent)
+
+		// ASSERT
+		assert.fieldEquals('Bounty', Constants.id, 'bountyId', Constants.bountyId)
+		assert.fieldEquals('Bounty', Constants.id, 'organization', Constants.organization)
+		assert.fieldEquals('Bounty', Constants.id, 'issuer', Constants.userId)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyAddress', Constants.id)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyMintTime', Constants.bountyMintTime)
+		assert.fieldEquals('Bounty', Constants.id, 'bountyType', Constants.bountyType_TIERED_FIXED)
+		assert.fieldEquals('Bounty', Constants.id, 'version', Constants.VERSION_4)
+		assert.fieldEquals('Bounty', Constants.id, 'transactionHash', Constants.transactionHash)
+		assert.fieldEquals('Bounty', Constants.id, 'hasFundingGoal', 'false')
+		assert.fieldEquals('Bounty', Constants.id, 'payoutTokenAddress', Constants.payoutTokenAddress)
+		assert.fieldEquals('Bounty', Constants.id, 'invoiceable', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'kycRequired', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'supportingDocuments', 'true')
+		assert.fieldEquals('Bounty', Constants.id, 'externalUserId', Constants.externalUserId)
 
 		// This is brittle - relies on proper spacing in the stringified array
 		assert.fieldEquals('Bounty', Constants.id, 'payoutSchedule', `[${Constants.payoutSchedule[0]}, ${Constants.payoutSchedule[1]}]`)
